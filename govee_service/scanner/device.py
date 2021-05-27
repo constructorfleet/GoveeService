a397a6d31876094098103ce57f8e3fc80cbf8713
Teagan glenn <teagan.m.glenn@gmail.com>
2021-05-27 00:04:42 -0600

    Use handle

--- govee_service/scanner/device.py
+++ govee_service/scanner/device.py
@@ -73,7 +73,7 @@ class Device:
         frame += bytes([checksum & 0xFF])
 
         async with BleakClient(self.address) as client:
-            await client.write_gatt_char('00010203-0405-0607-0809-0a0b0c0d2b11', frame)
+            await client.write_gatt_char(20, frame)
 
 
 class LedDevice(Device):
