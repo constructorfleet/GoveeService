76909c044c09177b76897437f747d34c0f48269e
Teagan glenn <teagan.m.glenn@gmail.com>
2021-05-26 23:53:42 -0600

    Turn off after scan

--- govee_service/__main__.py
+++ govee_service/__main__.py
@@ -4,20 +4,20 @@ import logging
 from scanner import Scanner, DEVICE_DISCOVERED
 
 logging.basicConfig(level=logging.INFO)
+scanner = Scanner()
 
 
 async def color_callback(data):
-    await data['device'].set_color((255, 255, 255))
+    await data['device'].turn_off((255, 255, 255))
 
 
 async def run():
-    scanner = Scanner()
-    scanner.on(DEVICE_DISCOVERED,
-               color_callback)
+    # scanner.on(DEVICE_DISCOVERED,
+    #            color_callback)
     await scanner.start()
     await asyncio.sleep(120)
     await scanner.stop()
-    # await scanner.known_devices[0].set_color((255, 255, 255))
+    await scanner.known_devices[0].set_color((255, 255, 255))
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
